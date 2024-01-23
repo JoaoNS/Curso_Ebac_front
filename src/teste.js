94919290b3f14901bbfe7320b8e65bbe00ac1872
JoaoNS <joaopnunes252@gmail.com>
2024-01-23 01:38:09 -0300

    exercicio_ts

---
+++ src/teste.js
@@ -0,0 +1,9 @@
+"use strict";
+function multi(a, b) {
+    return a * b;
+}
+function concat(a) {
+    return "Olá " + a;
+}
+console.log(multi(5, 6));
+console.log(concat("typescript"));
---
+++ src/teste.ts
@@ -0,0 +1,10 @@
+function multi (a:number, b:number):number {
+    return a * b;
+}
+
+function concat (a:string):string {
+    return "Olá " + a;
+}
+
+multi(6,5);
+concat("typescript!");
